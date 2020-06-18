599a0ea253686444b7652856f5f0c7f6184470a2
wujekstaszek <Olek1234>
2020-06-18 15:39:50 +0200

    upload to github

---
+++ ConsoleApplication3/CLOB.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Text;
+
+namespace CLOB
+{
+
+    public class CLOB
+    {
+        string connectionString, table, text, title;
+        public CLOB(string _connectionString, string _table, string _text, string _title)
+        {
+            connectionString = _connectionString;
+            table = _table;
+            text = _text;
+            title = _title;
+        }
+
+
+
+
+        public string[] find(string textToFind)
+        {
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string queryString = "SELECT "+title+" FROM "+table+" WHERE CONTAINS( "+text+" , @textToFind )";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.AddWithValue("@textToFind", textToFind);
+                SqlDataReader reader = command.ExecuteReader();
+                var toReturn=new List<string>();
+                int i=0;
+                while (reader.Read()){
+                    toReturn.Add((string)reader.GetValue(0));
+                    i++;}
+                reader.Close();
+                return toReturn.ToArray();
+            }
+        }
+        public bool addDocument(string path, string name)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                String text2 = File.ReadAllText(path);
+                string queryString = "INSERT INTO "+table+"("+title+","+text+") VALUES (@title2,@text2)";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.AddWithValue("@title2", name);
+                command.Parameters.AddWithValue("@text2", text2);
+                command.ExecuteNonQuery();
+            }
+            return true;
+
+        }
+
+        public bool deleteDocumentWith(string textToFind)
+        {
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string queryString = "SELECT " + title + " FROM " + table + " WHERE CONTAINS( " + text + " , @textToFind )";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.AddWithValue("@textToFind", textToFind);
+
+                SqlDataReader toDelete = command.ExecuteReader();
+                var toDeleteList = new List<string>();
+                int i = 0;
+                while (toDelete.Read())
+                {
+                    toDeleteList.Add((string)toDelete.GetValue(0));
+                    i++;
+                }
+                toDelete.Close();
+
+                queryString = "DELETE FROM "+table+" WHERE "+title+" = @title2";
+                command = new SqlCommand(queryString, connection);
+                foreach (var singleToDelte in toDeleteList)
+                {
+                    command.Parameters.AddWithValue("@title2", singleToDelte);
+                    command.ExecuteNonQuery();
+                    command.Parameters.RemoveAt("@title2");
+                }
+            }
+            return true;
+        }
+
+        public bool deleteDocument(string name)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string queryString = "DELETE FROM " + table + " WHERE " + title + " = @title2";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.AddWithValue("@title2", name);
+                command.ExecuteNonQuery();
+            }
+            return true;
+        }
+        static void Main()
+        { }
+    }
+}
---
+++ TestProject1/Testy.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CLOB;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+
+namespace TestProject1
+{
+    [TestClass]
+    public class Testy
+    {
+        private string connectionString,table,topic,text;
+        CLOB.CLOB testClob;
+        public Testy()
+        {
+            connectionString = @"
+                Data Source=.;
+                Database=clob;
+                Integrated Security=True";
+            table = "testowa1";
+            text = "tresc";
+            topic = "nazwa";
+            testClob = new CLOB.CLOB(connectionString, table, text, topic);
+        }
+
+        private TestContext testContextInstance;
+
+
+
+        [TestMethod]
+        public void Test1_SprawdzenieWyszukiwanieUdane()
+        {
+            string[] wynik = testClob.find("TestowyCiag1");
+            Assert.AreEqual("TestowyDokument1",wynik[0]);
+        }
+
+        [TestMethod]
+        public void Test2_SprawdzenieWyszukiwanieNieudane()
+        {
+            string[] wynik = testClob.find("TestowyCiag1242544");
+            Assert.IsTrue(wynik.Length==0);
+        }
+
+        [TestMethod]
+        public void Test3_SprawdzenieDodaniaPliku()
+        {
+            testClob.addDocument("C:\\Users\\Administrator\\Documents\\Visual Studio 2008\\Projects\\ConsoleApplication3\\TestowyDokument1.txt","TestowyDokument");
+            Thread.Sleep(5);
+            string[] wynik = testClob.find("ZawartoscTestowegoDokumentu1");
+            Assert.AreEqual(wynik[0], "TestowyDokument");
+        }
+
+        [TestMethod]
+        public void Test4_SprawdzenieUsunieciaPliku()
+        {
+            testClob.addDocument("C:\\Users\\Administrator\\Documents\\Visual Studio 2008\\Projects\\ConsoleApplication3\\TestowyDokument2.txt", "TestowyDokument2");
+            Thread.Sleep(5);
+            testClob.deleteDocument("TestowyDokument2");
+            string[] wynik = testClob.find("ZawartoscTestowegoDokumentu2");
+            Assert.IsTrue(wynik.Length == 0);
+        }
+        [TestMethod]
+        public void Test5_SprawdzenieUsunieciaPlikuZWyszukiwaniem()
+        {
+            testClob.addDocument("C:\\Users\\Administrator\\Documents\\Visual Studio 2008\\Projects\\ConsoleApplication3\\TestowyDokument2.txt", "TestowyDokument2");
+            testClob.deleteDocumentWith("ZawartoscTestowegoDokumentu2");
+            string[] wynik = testClob.find("ZawartoscTestowegoDokumentu2");
+            Assert.IsTrue(wynik.Length == 0);
+        }
+    }
+}
